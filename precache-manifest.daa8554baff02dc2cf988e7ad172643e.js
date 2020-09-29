7894b9817387d5c92cb07ad45c1295c571666a3e
Omar Crespo <crespomerchano@gmail.com>
2020-09-28 19:59:26 -0500

    635bcce

---
+++ precache-manifest.daa8554baff02dc2cf988e7ad172643e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d449211b31c94000072",
+    "url": "/comic-finder/css/app.9c52614f.css"
+  },
+  {
+    "revision": "4746fd3fd0904ee226e3af389416aff3",
+    "url": "/comic-finder/index.html"
+  },
+  {
+    "revision": "2d449211b31c94000072",
+    "url": "/comic-finder/js/app.2792ec02.js"
+  },
+  {
+    "revision": "b6591ca352350738e1ca",
+    "url": "/comic-finder/js/chunk-vendors.6a2ff2e4.js"
+  },
+  {
+    "revision": "958a2edbbc3ebec96408d91cc1d6e962",
+    "url": "/comic-finder/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/comic-finder/robots.txt"
+  }
+]);
